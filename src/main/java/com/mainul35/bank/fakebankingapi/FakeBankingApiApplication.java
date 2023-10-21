290c8463f4a77e3b0ac4893f4f05948e297e0bec
mainul35 <mainuls18@gmail.com>
2023-10-21 10:57:28 +0700

    first commit

---
+++ src/main/java/com/mainul35/bank/fakebankingapi/FakeBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.mainul35.bank.fakebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeBankingApiApplication.class, args);
+	}
+
+}
